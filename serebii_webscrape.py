6fd7d04ef61f4d27d45f8ba839551a9c23203ca3
Michael Au-Duong <mauduong723@gmail.com>
2020-11-14 13:27:23 +1300

    Alternate form typing scrape

--- serebii_webscrape.py
+++ serebii_webscrape.py
@@ -12,136 +12,136 @@ import re
 today = datetime.today().strftime('%Y%m%d%H%M%S')
 OUTPUT_FILE = ('pokemon_scrape_%s.json' % today)
 
-# TODO Generate an array for all pokemon in the galarian pokedex that is currently available
 def getPokemon():
     pokemonList = []
 
-    data = requests.get('https://www.serebii.net/swordshield/galarpokedex.shtml')
-    page = data.content
-    soup = BeautifulSoup(page, 'html.parser')
+    galarURL = 'https://www.serebii.net/swordshield/galarpokedex.shtml'
+    isleOfArmorURL = 'https://www.serebii.net/swordshield/isleofarmordex.shtml'
+    crownTundraURL = 'https://www.serebii.net/swordshield/thecrowntundradex.shtml'
+    legendariesURL = 'https://www.serebii.net/swordshield/pokemonnotindex.shtml'
 
-    try:
-        pokemonName = soup.find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
-    except Exception as ex:
-        print('Beautiful Soup scraping pokemon array error')
-        raise ex
+    urlList = [galarURL, isleOfArmorURL, crownTundraURL, legendariesURL]
 
-    pokemonNameLength = len(pokemonName)
-    for i in range(2, pokemonNameLength, 11):
-        pokemonList.append(pokemonName[i].find('a').contents[0].strip())
+    print('Attempting to scrape pokemon names...')
+    beforeExtractionTime = datetime.now()
 
-    print(pokemonList)
+    for url in urlList:
+        data = requests.get(url)
+        page = data.content
+        soup = BeautifulSoup(page, 'html.parser')
 
-def getData(urlArray):
-    try:
-        pokemonDetailList = []
+        try:
+            pokemonName = soup.find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
+        except Exception as ex:
+            print('Beautiful Soup scraping pokemon array error')
+            raise ex
 
-        for url in urlArray:
-            data = requests.get(url);
-            page = data.content
-            soup = BeautifulSoup(page, 'html.parser')
+        pokemonNameLength = len(pokemonName)
 
-            # Skip if pokemon not available
-            if (data.status_code != "200"):
-                break;
+        for hyperlink in range(2, pokemonNameLength, 11):
+            pokemonList.append(pokemonName[hyperlink].find('a').contents[0].strip())
 
-            try:
-                pokemonHeader = soup.find_all('td', attrs={'class': 'fooinfo'})
+    afterExtractionTime = datetime.now()
+    timeTaken = afterExtractionTime - beforeExtractionTime
+    print('Time taken to extract all pokemon names: {0}s'.format(timeTaken.seconds))
 
-                # Pokemon type
-                pokemonTypeTd = soup.find('td', attrs={'class': 'cen'})
-                pokemonTypeTr = pokemonTypeTd.find_all('tr')
+def getData(urlList):
 
-                # Alternative forms e.g Normal, Alolan, Galarian
-                pokemonTypeForm = {}
-                pokemonTypes = []
-                pokemonTypeList = []
+    pokemonDetailList = []
 
-                # TODO Fix the bullshit nested loop to properly display the correct form and type
-                for form in pokemonTypeTr:
-                    pokemonTypeForm['form'] = form.find_all('td')[0].text
-                    for type in form.find_all('img', alt=True):
-                        pokemonTypes.append(type.get('alt', '').replace('-type', '').lower())
-                        pokemonTypeForm['type'] = pokemonTypes
-                    # print(pokemonTypeForm)
-                    pokemonTypes = []
-                    pokemonTypeList.append(pokemonTypeForm)
-                    print(pokemonTypeList)
+    for url in urlList:
+        data = requests.get(url);
+        page = data.content
+        soup = BeautifulSoup(page, 'html.parser')
 
-                pokemonTypeImg = pokemonTypeTd.find_all('img')
-                # TODO Weakness and Resistances based on the alternative forms and types
-                # Weaknesses and Resistances
-                # pokemonTypingTable = soup.find_all('table', attrs={'class': 'dextable', 'style': '@media (max-width:1011px) {display:none;}'})
-                # pokemonTypingTr = pokemonTypingTable.find_all('tr')
-                # pokemonTypingTd = pokemonTypingTable.find_next_siblings('td', attrs={'class': 'footype'})
+        # Skip if pokemon not available
+        if (data.status_code != 200):
+            print('Serebii is currently down.')
+            break;
 
-                # Base stats
-                serebiiLastTd = soup.find_all('td', attrs={'class': 'fooevo', 'colspan': '4'})[-1]
+        try:
+            pokemonHeader = soup.find_all('td', attrs={'class': 'fooinfo'})
 
-                # print('Scraping {0}'.format(pokemonHeader[1].text))
-            except Exception as ex:
-                print('Beautiful Soup scraping data error')
-                raise ex
+            # Pokemon type
+            pokemonTypeTd = soup.find('td', attrs={'class': 'cen'})
+            pokemonTypeTr = pokemonTypeTd.find_all('tr')
 
-            # pokemonTypeList = []
-            # for type in pokemonTypeImg:
-            #     if type.get('alt', '').replace('-type', '').lower() not in pokemonTypeList:
-            #         pokemonTypeList.append(type.get('alt', '').replace('-type', '').lower())
+            # Alternative forms e.g Normal, Alolan, Galarian
+            pokemonForm = {}
+            pokemonTypeList = []
 
-            pokemon = {}
+            for form in pokemonTypeTr:
+                pokemonForm['form'] = form.find_all('td')[0].text
+                pokemonForm['type'] = ",".join([img['alt'].replace('-type', '').lower() for img in form.find_all('img', alt=True)])
+                pokemonDictionaryCopy = pokemonForm.copy()
+                pokemonTypeList.append(pokemonDictionaryCopy)
 
-            # Pokemon basic information
-            pokemon['name'] = pokemonHeader[1].text
-            pokemon['description'] = pokemonHeader[5].text
-            pokemon['type'] = pokemonTypeList
-            pokemon['ability'] = pokemonHeader[10].text.strip().replace(' (Available)', '').replace('\n', '')
-            pokemon['height'] = pokemonHeader[6].text.strip().replace('\r\n\t\t\t', '\n')
-            pokemon['weight'] = pokemonHeader[7].text.strip().replace('\r\n\t\t\t', '\n')
-            pokemon['captureRate'] = pokemonHeader[8].text
-            pokemon['stepsToHatchEgg'] = pokemonHeader[9].text
+            # pokemonTypeImg = pokemonTypeTd.find_all('img')
+            # TODO Weakness and Resistances based on the alternative forms and types
+            # Weaknesses and Resistances
+            # pokemonTypingTable = soup.find_all('table', attrs={'class': 'dextable', 'style': '@media (max-width:1011px) {display:none;}'})
+            # pokemonTypingTr = pokemonTypingTable.find_all('tr')
+            # pokemonTypingTd = pokemonTypingTable.find_next_siblings('td', attrs={'class': 'footype'})
 
-            # Pokemon typing weaknesses and resistances
-            # pokemon['resistance'] = []
-            # pokemon['weakness'] = []
+            # Base stats
+            serebiiLastTd = soup.find_all('td', attrs={'class': 'fooevo', 'colspan': '4'})[-1]
 
-            # Pokemon National, Galarian, Isle of Armor DLC Dex
-            pokemonDexList = pokemonHeader[3].text
-            pokemon['nationalNumber'] = re.search('[0-9]{3}|[-]{3}', pokemonDexList).group(0)
-            pokemon['galarNumber'] = re.search('Galar: #[0-9]{3}|[-]{3}|[Foreign]{7}', pokemonDexList).group(0).replace('Foreign', '---').replace('Galar: #', '')
-            pokemon['isleNumber'] = re.search('Isle of Armor: #[0-9]{3}|[-]{3}', pokemonDexList).group(0).replace('Isle of Armor: #', '')
+            # print('Scraping {0}'.format(pokemonHeader[1].text))
+        except Exception as ex:
+            print('Beautiful Soup scraping data error')
+            raise ex
 
-            # TODO Fix base stats based on alternative forms
-            # Hacky way to get pokemon base stats based on Gigantamax and multiple form
-            try:
-                # Gigantamax
-                if 'Picture' in serebiiLastTd:
-                    pokemonBaseStats = soup.find_all('table', {'class': 'dextable'})[-2].find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
-                # Non-Gigantamax
-                else:
-                    pokemonBaseStats = soup.find_all('table', {'class': 'dextable'})[-1].find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
-            except Exception as ex:
-                print('Error caught getting base stats: {0}'.format(ex))
-                raise ex
+        pokemon = {}
 
-            # Pokemon Base Stats
-            pokemon['hp'] = pokemonBaseStats[0].text
-            pokemon['attack'] = pokemonBaseStats[1].text
-            pokemon['defence'] = pokemonBaseStats[2].text
-            pokemon['spattack'] = pokemonBaseStats[3].text
-            pokemon['spdefence'] = pokemonBaseStats[4].text
-            pokemon['speed'] = pokemonBaseStats[5].text
+        # Pokemon basic information
+        pokemon['name'] = pokemonHeader[1].text
+        pokemon['description'] = pokemonHeader[5].text
+        pokemon['type'] = pokemonTypeList
+        pokemon['ability'] = pokemonHeader[10].text.strip().replace(' (Available)', '').replace('\n', '')
+        pokemon['height'] = pokemonHeader[6].text.strip().replace('\r\n\t\t\t', '\n')
+        pokemon['weight'] = pokemonHeader[7].text.strip().replace('\r\n\t\t\t', '\n')
+        pokemon['captureRate'] = pokemonHeader[8].text
+        pokemon['stepsToHatchEgg'] = pokemonHeader[9].text
 
-            # Pokemon Sword and Shield battle mechanic
-            pokemon['dynamax'] = 'cannot' not in pokemonHeader[14].text
-            pokemon['gigantamax'] = 'Gigantamax' in str(pokemonHeader[16])
+        # Pokemon typing weaknesses and resistances
+        # pokemon['resistance'] = []
+        # pokemon['weakness'] = []
 
-            pokemonDetailList.append(pokemon)
+        # Pokemon National, Galarian, Isle of Armor DLC Dex
+        pokemonDexList = pokemonHeader[3].text
+        pokemon['nationalNumber'] = re.search('[0-9]{3}|[-]{3}', pokemonDexList).group(0)
+        pokemon['galarNumber'] = re.search('Galar: #[0-9]{3}|[-]{3}|[Foreign]{7}', pokemonDexList).group(0).replace('Foreign', '---').replace('Galar: #', '')
+        pokemon['isleNumber'] = re.search('Isle of Armor: #[0-9]{3}|[-]{3}', pokemonDexList).group(0).replace('Isle of Armor: #', '')
 
-        # print(pokemonDetailList)
-        # saveData(pokemonDetailList)
-    except Exception as ex:
-        print('Error caught {0} for URL - {1}\n'.format(ex, url))
-        raise ex
+        # TODO Fix base stats based on alternative forms
+        # Hacky way to get pokemon base stats based on Gigantamax and multiple form
+        try:
+            # Gigantamax
+            if 'Picture' in serebiiLastTd:
+                pokemonBaseStats = soup.find_all('table', {'class': 'dextable'})[-2].find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
+            # Non-Gigantamax
+            else:
+                pokemonBaseStats = soup.find_all('table', {'class': 'dextable'})[-1].find_all('td', attrs={'align': 'center', 'class': 'fooinfo'})
+        except Exception as ex:
+            print('Error caught getting base stats: {0}'.format(ex))
+            raise ex
+
+        # Pokemon Base Stats
+        pokemon['hp'] = pokemonBaseStats[0].text
+        pokemon['attack'] = pokemonBaseStats[1].text
+        pokemon['defence'] = pokemonBaseStats[2].text
+        pokemon['spattack'] = pokemonBaseStats[3].text
+        pokemon['spdefence'] = pokemonBaseStats[4].text
+        pokemon['speed'] = pokemonBaseStats[5].text
+
+        # Pokemon Sword and Shield battle mechanic
+        pokemon['dynamax'] = 'cannot' not in pokemonHeader[14].text
+        pokemon['gigantamax'] = 'Gigantamax' in str(pokemonHeader[16])
+
+        pokemonDetailList.append(pokemon)
+
+    print(pokemonDetailList)
+    # saveData(pokemonDetailList)
 
 def saveData(pokemonList):
     with open(OUTPUT_FILE, 'a') as output_file:
@@ -149,12 +149,12 @@ def saveData(pokemonList):
 
 if __name__ == '__main__':
     try:
-        getPokemon()
-        # pokemonList = ['zacian'] #, 'slowpoke', 'zacian', 'venusaur'] #, 'thwackey', 'rillaboom'] #, 'zacian', 'zamazenta']
-        # urlList = ['https://www.serebii.net/pokedex-swsh/{}/'.format(pokemonList[p])
-        #     for p in range(len(pokemonList))]
+        # getPokemon()
+        pokemonList = ['zacian'] #, 'slowpoke', 'zacian', 'venusaur'] #, 'thwackey', 'rillaboom'] #, 'zacian', 'zamazenta']
+        urlList = ['https://www.serebii.net/pokedex-swsh/{}/'.format(pokemonList[pokemon])
+            for pokemon in range(len(pokemonList))]
 
-        # getData(urlList)
+        getData(urlList)
     except Exception as ex:
         print('Error caught during main')
         raise ex
